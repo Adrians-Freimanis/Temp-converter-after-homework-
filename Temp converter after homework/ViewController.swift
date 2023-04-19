838a1c1ff0e1306af7458dfe8c6a6c75075571c4
Adrians-Freimanis <adrians.freimanis@accenture.com>
2023-04-19 12:01:25 +0300

    Initial Commit

---
+++ Temp converter after homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Temp converter after homework
+//
+//  Created by adrians.freimanis on 19/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
